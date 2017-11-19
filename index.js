0c543a64d86fff11f0dc1c9dc01e037d6d6a1abf
Adrien Coppola <adrien.coppola@gmail.com>
2017-11-19 15:40:25 +0100

    some refactoring

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const fs = require('fs');
 
 /*
 // '+1' because ids begin with 1
-const usersCount = 1682+1:
+const usersCount = 1682+1;
 const itemsCount = 943+1;
 */
 
 const usersCount = 5+1;
 const itemsCount = 14+1;
+const datasetName = "tiny";
 
-fs.readFile('datasets/tiny.base', 'utf8', function (err,baseData) {
+fs.readFile(`datasets/${datasetName}.base`, 'utf8', function (err,baseData) {
   if (err) {
     return console.log(err);
   }
 
-  let inputMatrix = parseDatasetLines(usersCount, itemsCount, 0, baseData.split('\n'));
+  let inputMatrix = parseDatasetLines(usersCount, itemsCount, 0, baseData);
 
   //Run matrix computations
   let start = Date.now();
   var model = Recommender.buildModel(inputMatrix);
   let duration = Date.now() - start;
 
-  console.log("Model built in " + duration + "ms");
+  console.log(`Model built in ${duration}ms`);
 
   //formatting results of the model recommendations in one big matrix
   let bigDataMachineDeepLearningMatrixOfDoom = initMatrix(usersCount, itemsCount, 0);
@@ -42,24 +43,24 @@ fs.readFile('datasets/tiny.base', 'utf8', function (err,baseData) {
         // let's say that 2 decimals are enough
         bigDataMachineDeepLearningMatrixOfDoom[user][objectId] = Math.round(100 * rate) / 100;
       }
-
     }
   }
 
   //recommendations evaluation
-  fs.readFile('datasets/tiny.test', 'utf8', (err, testData) => {
+  fs.readFile(`datasets/${datasetName}.test`, 'utf8', (err, testData) => {
     if (err) {
       return console.log(err);
     }
 
     //recommendations matrix parsing
-    let perfectRecomMatrix = parseDatasetLines(usersCount, itemsCount, 0, testData.split('\n'));
+    let perfectRecomMatrix = parseDatasetLines(usersCount, itemsCount, 0, testData);
 
     let cosineSimilarities = new Array(usersCount);
     for (let user = 1; user < usersCount; user++){
       cosineSimilarities[user] = similarity( bigDataMachineDeepLearningMatrixOfDoom[user], perfectRecomMatrix[user] )
     }
 
+    /*
     console.log("perfects recommendations matrix");
     console.log(perfectRecomMatrix);
     console.log("\r\n recommendation");
@@ -67,6 +68,7 @@ fs.readFile('datasets/tiny.base', 'utf8', function (err,baseData) {
     console.log("\r\n cosine similarities (1 is the best, 0 is the worst");
     console.log("1st item is empty, because user 0 doesn't exist, it's a shift if the ids");
     console.log(cosineSimilarities);
+    */
   });
 });
 
@@ -90,8 +92,9 @@ function initMatrix(rows, cols, value){
 }
 
 // parse the lines of the dataset, and return a rows*cols matrix
-function parseDatasetLines(rows, cols, value, lines){
+function parseDatasetLines(rows, cols, value, data){
   let returnMatrix = initMatrix(rows, cols, value);
+  let lines = data.split('\n');
 
   for(let line of lines){
     //we don't want to handle empty lines
