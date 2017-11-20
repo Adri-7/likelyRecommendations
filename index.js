04e7ac6896a35c0cf38a2bc9b794fbdf7be7398c
Adrien Coppola <adrien.coppola@gmail.com>
2017-11-20 20:40:10 +0100

    MISC - tidy and clean up the code

--- index.js
+++ index.js
@@ -1,114 +1,131 @@
+// -----------------------------------------------------------------------------
+//                                   Imports
+// -----------------------------------------------------------------------------
 const Recommender = require('likely');
 const similarity = require( 'compute-cosine-similarity' );
 const fs = require('fs');
 
-// '+1' because ids begin with 1
-const USERS_COUNT = 943+1;
-const ITEMS_COUNT = 1682+1;
-const DATASETS_NAMES = ['u1', 'u2', 'u3', 'u4', 'u5']; //Choose you dataset here
 
+// -----------------------------------------------------------------------------
+//                                   Const
+// -----------------------------------------------------------------------------
+const USERS_COUNT = 5//943;
+const ITEMS_COUNT = 14//1682;
+//Choose your datasets here (don't forget to update dimensions if needed)
+const DATASETS_NAMES = ["tiny"]//['u1', 'u2', 'u3', 'u4', 'u5'];
+
+
+// -----------------------------------------------------------------------------
+//                                   main
+// -----------------------------------------------------------------------------
 for(name of DATASETS_NAMES){
-  processDataset(name);
+  //TODO maybe we could handle promises
+  processDataset(name)
+    .catch((err) => {
+      console.error(err);
+    });
 }
+
 
 // -----------------------------------------------------------------------------
 //                             Helper functions
 // -----------------------------------------------------------------------------
 
-//Run evaluation for dataset 'name'
+/**
+ * processDataset - run computations on datasets/'name'.base and compare with
+ *  datasets/'name'.test to evaluate
+ */
 function processDataset(name){
-  let BASE_FILE = `datasets/${name}.base`;
-  let TEST_FILE = `datasets/${name}.test`;
-  let RESULT_FILE = `logs/${name}.result`;
+  let baseFile = `datasets/${name}.base`;
+  let testFile = `datasets/${name}.test`;
+  let resultFile = `logs/${name}.result`;
 
   return new Promise((resolve, reject) => {
-    fs.readFile(BASE_FILE, 'utf8', function (err,baseData) {
+    fs.readFile(baseFile, 'utf8', function (err,baseData) {
       if (err) {
         reject(err);
-      } else {
-        console.log(`Parsing ${name} base file...`);
-        let inputMatrix = parseDatasetLines(USERS_COUNT, ITEMS_COUNT, 0, baseData);
+        return;
+      }
 
-        //Run matrix computations
-        console.log(`Running ${name} computations...`);
-        let start = Date.now();
-        let model = Recommender.buildModel(inputMatrix);
-        let duration = Date.now() - start;
+      console.log(`Parsing ${name}.base file...`);
+      let inputMatrix = parseDatasetLines(USERS_COUNT, ITEMS_COUNT, baseData);
+      console.log("done");
 
-        console.log(`Model built in ${duration}ms`);
+      console.log(`Running ${name} model computations...`);
+      let start = Date.now();
+      let model = Recommender.buildModel(inputMatrix);
+      let duration = Date.now() - start;
+      console.log(`done in ${duration}ms`);
 
-        //formatting results of the model recommendations in one big matrix
-        let bigDataMachineDeepLearningMatrixOfDoom = initMatrix(USERS_COUNT, ITEMS_COUNT, 0);
-        for (let user = 1; user < USERS_COUNT; user++){
+      //TODO rewrite this part to keep only pertinent data
+      //formatting results of the model recommendations in one big matrix
+      let bigDataMachineDeepLearningMatrixOfDoom = initMatrix(USERS_COUNT, ITEMS_COUNT, 0);
+      for (let user = 1; user < USERS_COUNT; user++){
 
-          var recommendationOfDoom = model.recommendations(user);
-          //console.log(recommendationOfDoom);
+        var recommendationOfDoom = model.recommendations(user);
+        //console.log(recommendationOfDoom);
 
-          for (let recommendation in recommendationOfDoom){
+        for (let recommendation in recommendationOfDoom){
 
-            let objectId = recommendationOfDoom[recommendation][0];
+          let objectId = recommendationOfDoom[recommendation][0];
 
-            // because of index shift
-            if (objectId != 0){
-              let rate = recommendationOfDoom[recommendation][1];
-              // let's say that 2 decimals are enough
-              bigDataMachineDeepLearningMatrixOfDoom[user][objectId] = Math.round(100 * rate) / 100;
-            }
+          // because of index shift
+          if (objectId != 0){
+            let rate = recommendationOfDoom[recommendation][1];
+            // let's say that 2 decimals are enough
+            bigDataMachineDeepLearningMatrixOfDoom[user][objectId] = Math.round(100 * rate) / 100;
           }
         }
-
-        //recommendations evaluation
-        fs.readFile(TEST_FILE, 'utf8', (err, testData) => {
-          if (err) {
-            reject(err);
-          } else {
-            //Make sur that we won't append in a non empty file
-            if(fs.existsSync(RESULT_FILE)){
-              fs.unlink(RESULT_FILE, (err) => {
-                if(err) reject(err);
-              });
-            }
-
-            //recommendations matrix parsing
-            let perfectRecomMatrix = parseDatasetLines(USERS_COUNT, ITEMS_COUNT, 0, testData);
-            let cosineSimilarities = new Array(USERS_COUNT);
-
-            for (let user = 1; user < USERS_COUNT; user++){
-              //Cosine similarity between recommendations and real ratings
-              cosineSimilarities[user] = similarity( bigDataMachineDeepLearningMatrixOfDoom[user], perfectRecomMatrix[user] );
-
-              //We then write it to a file
-              let line = cosineSimilarities[user] + '\n';
-
-              fs.appendFile(RESULT_FILE, line, (err) => {
-                if (err) reject(err);
-              });
-            }
-
-            /*
-            console.log("perfects recommendations matrix");
-            console.log(perfectRecomMatrix);
-            console.log("\r\n recommendation");
-            console.log(bigDataMachineDeepLearningMatrixOfDoom);
-            console.log("\r\n cosine similarities (1 is the best, 0 is the worst");
-            console.log("1st item is empty, because user 0 doesn't exist, it's a shift if the ids");
-            console.log(cosineSimilarities);
-            */
-
-            resolve();
-          }
-        });
       }
+
+      //recommendations evaluation
+      fs.readFile(testFile, 'utf8', (err, testData) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        //Make sur that we won't append in a non empty file
+        if(fs.existsSync(resultFile)){
+          fs.unlink(resultFile, (err) => {
+            if(err){
+              reject(err);
+            }
+          });
+        }
+
+        //recommendations matrix parsing
+        let perfectRecomMatrix = parseDatasetLines(USERS_COUNT, ITEMS_COUNT, testData);
+        let cosineSimilarities = new Array(USERS_COUNT);
+
+        for (let user = 1; user < USERS_COUNT; user++){
+          //Cosine similarity between recommendations and real ratings
+          cosineSimilarities[user] = similarity( bigDataMachineDeepLearningMatrixOfDoom[user], perfectRecomMatrix[user] );
+
+          //We then write it to the file
+          let line = cosineSimilarities[user] + '\n';
+
+          fs.appendFile(resultFile, line, (err) => {
+            if (err) reject(err);
+          });
+        }
+
+        //If we got here, then nothing wrong happened
+        resolve();
+      });
     });
   });
 }
 
-// init a matrix rows*cols with value
+
+/**
+ * initMatrix - init a matrix rows*cols with value
+ */
 function initMatrix(rows, cols, value){
-  let matrix = Array();
+  let matrix = [];
 
   for(let i = 0; i < rows; i++){
-    matrix.push(new Array());
+    matrix.push([]);
 
     for(let j = 0; j < cols; j++){
       matrix[i].push(0);
@@ -118,19 +135,24 @@ function initMatrix(rows, cols, value){
   return matrix;
 }
 
-// parse the lines of the dataset, and return a rows*cols matrix
-function parseDatasetLines(rows, cols, value, data){
-  let returnMatrix = initMatrix(rows, cols, value);
+
+/**
+ * parseDatasetLines - parses the dataset, and return a rows*cols matrix
+ */
+function parseDatasetLines(rows, cols, data){
+  let resultMatrix = initMatrix(rows, cols, 0);
   let lines = data.split('\n');
 
   for(let line of lines){
     //we don't want to handle empty lines
     if(line){
-      let [u, i, r, t] = line.split('\t');
+      let [user, item, rating, timestamp] = line.split('\t');
+      let i = parseInt(user)-1;
+      let j = parseInt(item)-1;
 
-      returnMatrix[parseInt(u)][parseInt(i)] = parseInt(r);
+      resultMatrix[i][j] = parseInt(rating);
     }
   }
 
-  return returnMatrix;
+  return resultMatrix;
 }
